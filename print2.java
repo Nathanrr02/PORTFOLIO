ce0d788965a193989735a9791e67634c5adb0d0d
Nathanrr02 <102532252+Nathanrr02@users.noreply.github.com>
2022-04-11 11:25:06 -0300

    Atividade de fixação VIM

---
+++ print2.java
@@ -0,0 +1,27 @@
+mkdir musica
+clear
+cd musica
+cd mpb
+touch é_isso_at.txt
+cd ..
+cd pop
+touch amet_te_ver
+cd ../rock
+touch iron_man.txt
+cd../sertanejo
+cd ../sertanejo
+touch solteiro_nao_trat.txt
+vim solteiro_nao_trai.txt
+sudo apt install vim
+sudo rm /var/lib/dpkg/lock-fronted
+sudo rm /var/lib/dpkg/lock-frontend
+sudo dpkg --configure -a
+sudo rm -rf /var/lib/apt/list/lock
+sudo rm /var/lib/dpkg/lock
+sudo rm /var/cache/apt/archtves/lock
+sudo dpkg --configure -a
+sudo apt update
+sudo apt install vim
+vim solteiro_nao_trai.txt
+vim iron_man.txt
+vim amet_te_ver.txt
